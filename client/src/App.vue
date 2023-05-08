51811d51266dcdbf6f2a71cdbd6fcb8a6c44fee8
Lamiae Abdjalille <lamiae.github@gmail.com>
2023-05-08 23:35:05 +0100

    Add router configuration

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+
+
 </script>
 
 <template>
-  
+ 
+    <router-view>
+    
+  </router-view> 
+ 
+ 
+
+
 </template>
+
+<style scoped>
+
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,8 +3,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+   
+    {
+    path: '/access/:method/',
+    name: 'Access',
+    component: () => import('../views/Access.vue'),
+    },
   ]
+  
 })
+
+
+
+
 
 export default router
